391179921174e14385d665e02e74cd1060fe8088
kestertan <kestertan040@gmail.com>
2022-10-16 05:57:05 -0700

    create and get functions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const _ = require("lodash");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 const { PrismaClient } = require("@prisma/client");
 const { v4: uuidv4 } = require("uuid");
 
@@ -10,3 +18,53 @@ const prisma = new PrismaClient({
     },
   },
 });
+
+
+async function createUser(email, name, password, profilePic) {
+  user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password,
+      profilePic: profilePic ?? ''
+    }
+  })
+
+  return user;
+
+};
+
+async function getUser(userId) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  })
+
+  return user 
+};
+
+async function createEntry(userId, content, lat, long) {
+  user = await prisma.entry.create({
+    data: {
+      userId,
+      content,
+      lat,
+      long
+    }
+  })
+}
+
+async function getEntryById(entryId) {
+  const entry = await prisma.entry.findUnique({
+    where: {
+      id: entryId
+    }
+  })
+}
+
+async function getAllEntries() {
+  const entries = await prisma.entry.findMany();
+}
+
+app.get("/")
